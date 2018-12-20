5443b247a11bfe50f6e54c55ff742bbace92a25a
qp-love-znn <45379278+qp-love-znn@users.noreply.github.com>
2018-12-20 14:25:29 +0800

    Update Extract_seq.pm

--- Extract_seq.pm
+++ Extract_seq.pm
@@ -13,6 +13,11 @@ our @version = 1.0;
 #get_gene_seq : get the gene seq--->$self->get_gene_seq($fa,\%genes_posit)
 #get_gene_start_cds:get the gene cds seq --->$self->get_gene_start_cds($gtf,$fa)
 
+#2018-12-20 add
+#get_2k_seq : get the lnc before 2k seq --->$self->get_2k_seq($gtf,$fa)
+#get_line_seq : convert the seq to line --->$self->get_line_seq($fa)
+#get_target_pos : --->$self->get_target_pos($fa)
+
 sub new{
 	my $class = shift;
 	my $self = {};
@@ -137,5 +142,86 @@ sub get_gene_start_cds{
 	close IN1;
 }
 
+sub get_2k_seq{
+	my $self = shift;
+	my ($gtf,$fa) = @_;
+	open IN,$gtf or die $!;
+	my (%lnc_posit,%strand);
+	while(<IN>){
+		chomp;
+		my @t = split /\t/,$_;
+		my ($g) = $t[8] =~ /transcript_id \"([^;]+)\";/;
+		$strand{$g} = $t[6];
+		if(exists $lnc_posit{$t[0]}{$g}){
+			if($lnc_posit{$t[0]}{$g} > $t[3]){
+				$lnc_posit{$t[0]}{$g} = $t[3];
+			}
+		} else {
+			$lnc_posit{$t[0]}{$g} = $t[3];
+		}
+	}
+	close IN;
+	open IN1,$fa or die $!;
+	$/=">";
+	<IN1>;
+	while(<IN1>){
+		chomp;
+		my @t = split /\n/,$_,2;
+		my ($name) = $t[0] =~ /(^\S+)/;
+		$t[1] =~ s/\n//g;
+		if(exists $lnc_posit{$name}){
+			for my $g (keys %{$lnc_posit{$name}}){
+				my $st = $lnc_posit{$name}{$g} - 2000;
+				my $seq = substr($t[1],$st,2000);
+				if($strand{$g} eq "-"){
+					$seq =~ tr/ATGCatgc/TACGtacg/;
+					print ">$g\n".reverse($seq)."\n";
+				} else {
+					print ">$g\n$seq\n";
+				}
+			}
+		}
+	}
+	close IN1;
+}
+
+sub get_line_seq{
+	my $self = shift;
+	my $fa = @_;
+	open IN,$fa or die $!;
+	$/=">";
+	<IN>;
+	while(<IN>){
+		chomp;
+		my @t = split /\n/,$_,2;
+		$t[1] =~ s/\n//g;
+		print ">$t[0]\n$t[1]\n";
+	}
+	close IN;
+}
+
+sub get_target_pos{
+	my $self = shift;
+	my ($fa) = @_;
+	open IN,$fa or die $!;
+	$/=">";
+	<IN>;
+	while(<IN>){
+		chomp;
+		my @t = split /\n/,$_,2;
+		$t[1] =~ s/\n//g;
+		$t[1] = uc($t[1]);
+#		my @arrseq = $t[1] =~ /([ATGC]G[ATGC]GG[ATGC][ATGC]GGAG[ATGC])/g;
+		my @posit;
+		my @arrseq = $t[1] =~ /RPMNAFMVW/g;
+		while($t[1] =~ /RPMNAFMVW/g){
+			push @posit,pos($t[1]);
+		}
+		if(@arrseq){
+			print "$t[0]\t".join(",",@arrseq)."\t".join(",",@posit)."\t$t[1]\n";
+		}
+	}
+	close IN;
+}
 
 1;
